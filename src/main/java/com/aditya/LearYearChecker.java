c808784b02d0bfa0c840f447c17a799f4e33b79a
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-02 15:51:16 +0530

    checked the leap year in main method

--- src/main/java/com/aditya/LearYearChecker.java
+++ src/main/java/com/aditya/LearYearChecker.java
@@ -6,4 +6,23 @@
 package com.aditya;
 
 public class LearYearChecker {
+    public static void main(String[] args) {
+
+        // Create an object of the class
+        LearYearChecker learYearChecker = new LearYearChecker();
+        learYearChecker.isLeapYear(2012);
+
+        // print the result
+        System.out.println(learYearChecker.isLeapYear(2012));
+
+    }
+        /*
+         * This method checks if the given year is a leap year.
+         *
+         * @param year the year to check for leap year.
+         * @return true if the year is a leap year, false otherwise.
+         */
+        public boolean isLeapYear(int year) {
+            return year % 4 == 0;
+        }
 }
